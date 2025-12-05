a2f22fbf3f5773be8b322828f0ca92f2bf4ccf39
ahmed111 <ahmedeyhab521@gmail.com>
2025-12-05 13:46:49 +0200

    fix: index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,18 @@
 import { addTask } from "./addTask.js";
 import { darkModeToggle } from "./darkModeToggle.js";
-import { deleteTask } from "./deleteTask.js";
-import { Elements } from "./Elements.js";
+import {
+  addButton,
+  clearBtn,
+  filterActiveBtn,
+  filterAllBtn,
+  filterBtns,
+  filterCompletedBtn,
+  itemLeft,
+  toggleButton,
+} from "./Elements.js";
 import { fetchData } from "./fetchData.js";
 import { initDataStartUP } from "./initDataStartUP.js";
-import { initTaskListeners } from "./initTaskListeners.js";
-import { renderTasks } from "./reanderTasks.js";
+import { reanderTasks } from "./reanderTasks.js";
 import { saveTODB } from "./saveTODB.js";
 
 toggleButton.addEventListener("click", () => darkModeToggle());
